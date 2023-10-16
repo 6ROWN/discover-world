64c5ab6f27668f2376bfa21cda77d28fd00935d3
Daniel Gohoho <gohohodanni@gmail.com>
2023-10-16 12:58:28 +0000

    Refactor code structure

--- utils/request.ts
+++ utils/request.ts
@@ -2,17 +2,21 @@ export const photoBaseUrl =
 	"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400";
 
 export const fetchPlaces = async (query: string) => {
-	const response = await fetch(`api/google-place-api?query=${query}`, {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
+	try {
+		const response = await fetch(`api/google-place-api?query=${query}`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
 
-	if (!response.ok) {
-		throw new Error("Failed to fetch places");
+		if (!response.ok) {
+			throw new Error("Failed to fetch places");
+		}
+
+		const result = await response.json();
+		return result.results;
+	} catch (error) {
+		throw error;
 	}
-
-	const result = await response.json();
-	return result.results;
 };
